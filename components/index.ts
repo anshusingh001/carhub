8b18c3587cfd4b4f9a1dcd0a6d89ea5780a0c814
Anshu Kumar Singh <anshusinghak001@gmail.com>
2024-04-20 17:12:35 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import CarDetail from "./CarDetail";
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    ShowMore,
+    SearchBar,
+    CustomFilter,
+    CarCard,
+    CarDetail
+};
